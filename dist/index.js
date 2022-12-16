7e106c410489b49d584ed100944a1f2f05b17948
Luis Sandoval <luismsandoval96@gmail.com>
2022-12-15 19:48:15 -0800

    dist environment

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const routes_1 = __importDefault(require("./routes/routes"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+const uri = process.env.URI;
+mongoose_1.default.connect(uri);
+app.use(express_1.default.json());
+app.use("/api/scores", routes_1.default);
+app.get("/", (req, res) => {
+    res.send("hello world this is a test");
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
