3eb9c13ae5d12fbbee9d33919bbff90acd2db3a3
Luis Sandoval <luismsandoval96@gmail.com>
2022-12-17 10:39:12 -0800

    cors allow origin

--- index.ts
+++ index.ts
@@ -13,6 +13,14 @@ mongoose.connect(uri!);
 
 app.use(express.json());
 app.use("/api/scores", router);
+app.use((req: Request, res: Response, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world this is a test");
